ae02e756eb3e42420a32f22d4f27c580c12de76d
AlfredoHdez1709 <alfredo_hernandez_rguez@outlook.com>
2017-05-12 22:48:45 -0500

    Se agrega Lab6

---
+++ Lab6/PhoneApp/MainActivity.cs
@@ -0,0 +1,84 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace PhoneApp
+{
+    [Activity(Label = "Phone App", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        static readonly System.Collections.Generic.List<string> PhoneNumbers = new System.Collections.Generic.List<string>();
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+            SetContentView(Resource.Layout.Main);
+
+          
+            var PhoneNumber = FindViewById<EditText>(Resource.Id.PhoneNumberText);
+            var TranslateButton = FindViewById<Button>(Resource.Id.TranslateButton);
+            var CallButton = FindViewById<Button>(Resource.Id.CallButton);
+            var CallHistorybutton = FindViewById<Button>(Resource.Id.CallHistoryButton);
+            var Validatebutton = FindViewById<Button>(Resource.Id.ValidateButton);
+            CallButton.Enabled = false;
+
+
+
+            var TranslatedNumber = string.Empty;
+
+
+            Validatebutton.Click += (sender, e) =>
+            {
+                var i = new Android.Content.Intent(this, typeof (ValidationClass));
+                StartActivity(i);
+            };
+
+            CallHistorybutton.Click += (sender, e) =>
+             {
+                 var i = new Android.Content.Intent(this, typeof(CallHistoryActivity));
+                 i.PutStringArrayListExtra("phone_numbers", PhoneNumbers);
+                 StartActivity(i);
+             };
+
+            TranslateButton.Click += (object sender, System.EventArgs e) =>
+            {
+                var Translator = new PhoneTranslator();
+                TranslatedNumber = Translator.ToNumber(PhoneNumber.Text);
+
+                if (string.IsNullOrWhiteSpace(TranslatedNumber))
+                {
+                    CallButton.Text = "Llamar";
+                    CallButton.Enabled = false;
+                }
+                else
+                {
+                    CallButton.Text = $"Llamar al {TranslatedNumber}";
+                    CallButton.Enabled = true;
+                }
+            };
+
+
+            CallButton.Click += (object sender, System.EventArgs e) =>
+            {
+
+                var CallDialog = new AlertDialog.Builder(this);
+                CallDialog.SetMessage($"Llamar al numero {TranslatedNumber}?");
+                CallDialog.SetNeutralButton("Llamar", delegate
+                {
+                    PhoneNumbers.Add(TranslatedNumber);
+                    CallHistorybutton.Enabled = true;
+
+                    var CallI = new Android.Content.Intent(Android.Content.Intent.ActionCall);
+                    CallI.SetData(Android.Net.Uri.Parse($"tel:{TranslatedNumber}"));
+                    StartActivity(CallI);
+                });
+                CallDialog.SetNegativeButton("Cancelar", delegate { });
+                CallDialog.Show();
+            };
+
+
+        }
+
+    }
+}
+
---
+++ Lab6/PhoneApp/ValidationClass.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace PhoneApp
+{
+    [Activity(Label = "@string/validatemsn")]
+    public class ValidationClass : Activity
+    {
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.ValidationActivity);
+
+           
+            var btnvalidate = FindViewById<Button>(Resource.Id.btnvalidateprueba);
+
+            btnvalidate.Click += Btnvalidate_Click;
+
+        }
+
+        private async void Btnvalidate_Click(object sender, EventArgs e)
+        {
+
+            var editemail = FindViewById<EditText>(Resource.Id.editemail);
+            var editpassword = FindViewById<EditText>(Resource.Id.editpassword);
+
+
+            var studentemail = string.Empty;
+            var password = string.Empty;
+
+            studentemail = (editemail.Text);
+            password = (editpassword.Text);
+
+            SALLab06.ServiceClient ServiceClient = new SALLab06.ServiceClient();
+
+
+            string myDevice = Android.Provider.Settings.Secure.GetString(
+
+                ContentResolver,
+
+                Android.Provider.Settings.Secure.AndroidId
+
+                );
+
+            SALLab06.ResultInfo Result = await ServiceClient.ValidateAsync(studentemail, password, myDevice);
+                        var txtvalidate = FindViewById<TextView>(Resource.Id.txtValidate);
+
+
+
+            txtvalidate.Text = $"{Result.Status}\n{Result.Fullname}\n{Result.Token}";
+        }
+    }
+}
